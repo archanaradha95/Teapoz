a3adf2635d763307d2744d749e16b0e843cdccfe
archanaradha95 <archanaradha95@gmail.com>
2025-09-02 11:21:39 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        marcellus: ['Marcellus', 'serif'],
+        custom: ['Sentient', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
